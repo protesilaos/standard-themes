e280a58cd8e32cf5bd32ced9fecce2cd88814a6c
Protesilaos Stavrou <info@protesilaos.com>
2022-11-17 11:31:53 +0200

    Add initial version of the standard-themes

---
+++ standard-dark-theme.el
@@ -0,0 +1,203 @@
+;;; standard-dark-theme.el --- Like the unthemed dark Emacs, but more consistent -*- lexical-binding:t -*-
+
+;; Copyright (C) 2022  Free Software Foundation, Inc.
+
+;; Author: Protesilaos Stavrou <info@protesilaos.com>
+;; Maintainer: Standard-Themes Development <~protesilaos/standard-themes@lists.sr.ht>
+;; URL: https://git.sr.ht/~protesilaos/standard-themes
+;; Mailing-List: https://lists.sr.ht/~protesilaos/standard-themes
+;; Keywords: faces, theme, accessibility
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; The `standard-themes' are a pair of light and dark themes for GNU
+;; Emacs.  They emulate the out-of-the-box looks of Emacs (which
+;; technically do NOT constitute a theme) while bringing to them
+;; thematic consistency, customizability, and extensibility.
+
+;;; Code:
+
+
+
+(eval-and-compile
+  (require 'standard-themes)
+
+  (deftheme standard-dark "Like the unthemed dark Emacs, but more consistent.")
+
+  (defconst standard-dark-palette
+    '(;; Basic tones
+      (bg-main     "#000000")
+      (fg-main     "#ffffff")
+      (bg-dim      "#202020")
+      (fg-dim      "#a6a6a6")
+      (bg-alt      "#323232")
+      (fg-alt      "#a0afef")
+
+      (bg-active   "#424242")
+      (bg-inactive "#141414")
+
+      ;; Basic hues for foreground values
+      (red             "#ff6f60")
+      (red-warmer      "#ff7f24")
+      (red-cooler      "#ff5a7a")
+      (red-faint       "#ee5c42")
+      (green           "#44cc44")
+      (green-warmer    "#7abd0f")
+      (green-cooler    "#98fb98")
+      (green-faint     "#61a06c")
+      (yellow          "#eedd82")
+      (yellow-warmer   "#fec43f")
+      (yellow-cooler   "#ffa07a")
+      (yellow-faint    "#cf9f8f")
+      (blue            "#87ceff")
+      (blue-warmer     "#6a9fff")
+      (blue-cooler     "#02cfff")
+      (blue-faint      "#b0c4de")
+      (magenta         "#ff8faf")
+      (magenta-warmer  "#e580ea")
+      (magenta-cooler  "#ee82ee")
+      (magenta-faint   "#c57faf")
+      (cyan            "#00ffff")
+      (cyan-warmer     "#87cefa")
+      (cyan-cooler     "#7fffd4")
+      (cyan-faint      "#8aa0df")
+
+      ;; Basic hues for background values
+      (bg-red      "#cd2f30")
+      (bg-green    "#408420")
+      (bg-yellow   "#8f5040")
+      (bg-blue     "#4648d0")
+      (bg-magenta  "#a050cf")
+      (bg-cyan     "#2270be")
+
+      ;; Diffs
+      (bg-added          "#00331f")
+      (bg-added-faint    "#002410")
+      (bg-added-refine   "#03492f")
+
+      (bg-changed        "#323200")
+      (bg-changed-faint  "#281a00")
+      (bg-changed-refine "#484800")
+
+      (bg-removed        "#4a1119")
+      (bg-removed-faint  "#320a0f")
+      (bg-removed-refine "#751a1f")
+
+      ;; Graphs
+      (red-graph-0-bg     "#b52c2c")
+      (red-graph-1-bg     "#702020")
+      (green-graph-0-bg   "#4fd100")
+      (green-graph-1-bg   "#007800")
+      (yellow-graph-0-bg  "#f1e00a")
+      (yellow-graph-1-bg  "#b08600")
+      (blue-graph-0-bg    "#2fafef")
+      (blue-graph-1-bg    "#1f2f8f")
+      (magenta-graph-0-bg "#bf94fe")
+      (magenta-graph-1-bg "#5f509f")
+      (cyan-graph-0-bg    "#47dfea")
+      (cyan-graph-1-bg    "#00808f")
+
+      ;; Special hues
+      (bg-mode-line  "#505050") (fg-mode-line  "#ffffff")
+      (bg-accent     "#ffc200")
+      (bg-completion "#334e22")
+      (bg-hover      "#457b2f")
+      (bg-hover-alt  "#00688b")
+      (bg-hl-line    "#556b2f")
+      (bg-region     "#0000cd")
+      (bg-paren      "#4f94cd")
+      (bg-err        "#3f0d09") ; check with err
+      (bg-warning    "#362f00") ; check with warning
+      (bg-info       "#002f12") ; check with info
+
+      (border        "#606070")
+      (cursor        "#ffffff")
+
+      (bg-tab              "#333333")
+      (bg-tab-inactive     "#4d4d4d")
+      (bg-tab-inactive-alt "#666666")
+
+      (modeline-err     "#ff80af")
+      (modeline-warning "#dfcf33")
+      (modeline-info    "#2fc82f")
+
+      (underline-err     "#df2f2f")
+      (underline-warning "#c0b000")
+      (underline-info    "#22b022")
+
+      ;; Conditional hues
+      (bg-region-intense   "#9f3047")
+      (bg-mode-line-accent "#662a10")
+
+      ;; Mappings
+      (err red)
+      (warning yellow-warmer)
+      (info green)
+
+      (link cyan)
+      (link-alt magenta-cooler)
+      (link-faint cyan-faint)
+      (link-alt-faint magenta-faint)
+      (date cyan-warmer)
+      (name cyan-cooler)
+      (keybind blue-cooler)
+      (prompt blue)
+      (bg-prompt bg-green)
+
+      (builtin blue-faint)
+      (comment red-warmer)
+      (constant cyan-cooler)
+      (fnname cyan-warmer)
+      (keyword cyan)
+      (preprocessor blue-faint)
+      (docstring yellow-cooler)
+      (string yellow-cooler)
+      (type green-cooler)
+      (variable yellow)
+
+      (rx-escape green) ; compare with `string'
+      (rx-construct fg-main)
+
+      (accent-0 yellow)
+      (accent-1 cyan-warmer)
+      (accent-2 green-cooler)
+      (accent-3 red-warmer)
+
+      (mail-0 yellow-cooler)
+      (mail-1 cyan-warmer)
+      (mail-2 cyan-cooler)
+      (mail-3 magenta-cooler)
+      (mail-4 red-warmer)
+      (mail-5 blue)
+
+      (rainbow-0 "#afeeee")
+      (rainbow-1 cyan-warmer)
+      (rainbow-2 yellow)
+      (rainbow-3 cyan)
+      (rainbow-4 red-warmer)
+      (rainbow-5 green-cooler)
+      (rainbow-6 cyan-cooler)
+      (rainbow-7 blue-faint)
+      (rainbow-8 yellow-cooler))
+    "The `standard-dark' palette.")
+
+  (standard-themes-theme standard-dark standard-dark-palette)
+
+  (provide-theme 'standard-dark))
+
+;;; standard-dark-theme.el ends here
---
+++ standard-light-theme.el
@@ -0,0 +1,203 @@
+;;; standard-light-theme.el --- Like the unthemed light Emacs, but more consistent -*- lexical-binding:t -*-
+
+;; Copyright (C) 2022  Free Software Foundation, Inc.
+
+;; Author: Protesilaos Stavrou <info@protesilaos.com>
+;; Maintainer: Standard-Themes Development <~protesilaos/standard-themes@lists.sr.ht>
+;; URL: https://git.sr.ht/~protesilaos/standard-themes
+;; Mailing-List: https://lists.sr.ht/~protesilaos/standard-themes
+;; Keywords: faces, theme, accessibility
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; The `standard-themes' are a pair of light and dark themes for GNU
+;; Emacs.  They emulate the out-of-the-box looks of Emacs (which
+;; technically do NOT constitute a theme) while bringing to them
+;; thematic consistency, customizability, and extensibility.
+
+;;; Code:
+
+
+
+(eval-and-compile
+  (require 'standard-themes)
+
+  (deftheme standard-light "Like the unthemed light Emacs, but more consistent.")
+
+  (defconst standard-light-palette
+    '(;; Basic tones
+      (bg-main     "#ffffff")
+      (fg-main     "#000000")
+      (bg-dim      "#ededed")
+      (fg-dim      "#7f7f7f")
+      (bg-alt      "#e5e5e5")
+      (fg-alt      "#193f8f")
+
+      (bg-active   "#bfbfbf")
+      (bg-inactive "#f5f5f5")
+
+      ;; Basic hues for foreground values
+      (red             "#b3303a")
+      (red-warmer      "#e00033")
+      (red-cooler      "#ee3b3b")
+      (red-faint       "#b22222")
+      (green           "#228b22")
+      (green-warmer    "#4f7400")
+      (green-cooler    "#008858")
+      (green-faint     "#61756c")
+      (yellow          "#a45f22")
+      (yellow-warmer   "#b6532f")
+      (yellow-cooler   "#a0522d")
+      (yellow-faint    "#a65f6a")
+      (blue            "#001faf")
+      (blue-warmer     "#3a5fcd")
+      (blue-cooler     "#0000ff")
+      (blue-faint      "#483d8b")
+      (magenta         "#8b008b")
+      (magenta-warmer  "#8b2252")
+      (magenta-cooler  "#800080")
+      (magenta-faint   "#bf3580")
+      (cyan            "#1f6fbf")
+      (cyan-warmer     "#2f8fab")
+      (cyan-cooler     "#008b8b")
+      (cyan-faint      "#506fa0")
+
+      ;; Basic hues for background values
+      (bg-red      "#ff8f88")
+      (bg-green    "#7ae070")
+      (bg-yellow   "#fac200")
+      (bg-blue     "#6bcfff")
+      (bg-magenta  "#cf9fff")
+      (bg-cyan     "#a8efff")
+
+      ;; Diffs
+      (bg-added          "#d5f8d5")
+      (bg-added-faint    "#e5ffe5")
+      (bg-added-refine   "#c6e8c6")
+
+      (bg-changed        "#ffdfa9")
+      (bg-changed-faint  "#ffefbf")
+      (bg-changed-refine "#fac090")
+
+      (bg-removed        "#ffd8d5")
+      (bg-removed-faint  "#ffe9e9")
+      (bg-removed-refine "#f3b5af")
+
+      ;; Graphs
+      (red-graph-0-bg     "#ef7969")
+      (red-graph-1-bg     "#ffaab4")
+      (green-graph-0-bg   "#4faa09")
+      (green-graph-1-bg   "#8fef00")
+      (yellow-graph-0-bg  "#ffcf00")
+      (yellow-graph-1-bg  "#f9ff00")
+      (blue-graph-0-bg    "#7090ff")
+      (blue-graph-1-bg    "#9fc6ff")
+      (magenta-graph-0-bg "#e07fff")
+      (magenta-graph-1-bg "#fad0ff")
+      (cyan-graph-0-bg    "#70d3f0")
+      (cyan-graph-1-bg    "#afefff")
+
+      ;; Special hues
+      (bg-mode-line  "#b3b3b3")  (fg-mode-line  "#000000")
+      (bg-accent     "#0000cc")
+      (bg-completion "#bfe8ff")
+      (bg-hover      "#aaeccf")
+      (bg-hover-alt  "#ffff00")
+      (bg-hl-line    "#b4eeb4")
+      (bg-region     "#eedc82")
+      (bg-paren      "#40e0d0")
+      (bg-err        "#ffd5ea") ; check with err
+      (bg-warning    "#ffeabb") ; check with warning
+      (bg-info       "#d0efda") ; check with info
+
+      (border        "#bababa")
+      (cursor        "#000000")
+
+      (bg-tab              "#d9d9d9")
+      (bg-tab-inactive     "#bfbfbf")
+      (bg-tab-inactive-alt "#a6a6a6")
+
+      (modeline-err     "#b02020")
+      (modeline-warning "#5f1080")
+      (modeline-info    "#002fb0")
+
+      (underline-err     "#ef0f1f")
+      (underline-warning "#bf5f00")
+      (underline-info    "#02af52")
+
+      ;; Conditional hues
+      (bg-region-intense   "#eeb422")
+      (bg-mode-line-accent "#9fbaff")
+
+      ;; Mappings
+      (err red-warmer)
+      (warning yellow-warmer)
+      (info green)
+
+      (link blue-warmer)
+      (link-alt magenta)
+      (link-faint blue-faint)
+      (link-alt-faint magenta-faint)
+      (date cyan-warmer)
+      (name cyan-cooler)
+      (keybind blue)
+      (prompt blue-cooler)
+      (bg-prompt bg-blue)
+
+      (builtin blue-faint)
+      (comment red-faint)
+      (constant cyan-cooler)
+      (fnname blue-cooler)
+      (keyword magenta-cooler)
+      (preprocessor blue-faint)
+      (docstring magenta-warmer)
+      (string magenta-warmer)
+      (type green)
+      (variable yellow-cooler)
+
+      (rx-escape green) ; compare with `string'
+      (rx-construct fg-main)
+
+      (accent-0 blue-cooler)
+      (accent-1 magenta)
+      (accent-2 red-faint)
+      (accent-3 yellow-cooler)
+
+      (mail-0 magenta-cooler)
+      (mail-1 blue-warmer)
+      (mail-2 cyan-cooler)
+      (mail-3 yellow-cooler)
+      (mail-4 blue)
+      (mail-5 green-warmer)
+
+      (rainbow-0 "#191970")
+      (rainbow-1 blue-cooler)
+      (rainbow-2 yellow-cooler)
+      (rainbow-3 magenta-cooler)
+      (rainbow-4 red-faint)
+      (rainbow-5 green-warmer)
+      (rainbow-6 cyan-cooler)
+      (rainbow-7 blue-faint)
+      (rainbow-8 magenta-warmer))
+    "The `standard-light' palette.")
+
+  (standard-themes-theme standard-light standard-light-palette)
+
+  (provide-theme 'standard-light))
+
+;;; standard-light-theme.el ends here

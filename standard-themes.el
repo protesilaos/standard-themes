7b0d09058e1d987e80b7324c940d9b81f3009ea3
Protesilaos Stavrou <info@protesilaos.com>
2024-02-26 10:07:53 +0200

    Do not underline the gnus-button

--- standard-themes.el
+++ standard-themes.el
@@ -1339,7 +1339,7 @@ Optional prefix argument MAPPINGS has the same meaning as for
     `(git-rebase-description ((,c :foreground ,fg-main)))
     `(git-rebase-hash ((,c :foreground ,constant)))
 ;;;; gnus
-    `(gnus-button ((,c :inherit button)))
+    `(gnus-button ((,c :inherit button :underline nil)))
     `(gnus-cite-1 ((,c :inherit message-cited-text-1)))
     `(gnus-cite-2 ((,c :inherit message-cited-text-2)))
     `(gnus-cite-3 ((,c :inherit message-cited-text-3)))

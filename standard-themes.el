9a982c4fad9a6d1a68e211bd83342bdd47dd7979
Protesilaos Stavrou <info@protesilaos.com>
2024-08-11 09:10:21 +0300

    Update to standard-themes version 2.1.0

--- standard-themes.el
+++ standard-themes.el
@@ -5,7 +5,7 @@
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; Maintainer: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://github.com/protesilaos/standard-themes
-;; Version: 2.0.1
+;; Version: 2.1.0
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: faces, theme, accessibility
 

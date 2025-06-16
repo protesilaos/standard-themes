61ce2354dea5d94ead590eaf65e48418aba57503
Protesilaos Stavrou <info@protesilaos.com>
2025-06-16 07:36:15 +0300

    Add support for the new spacious-padding face names
    I keep the old ones for the time being, but they will be removed eventually.

--- standard-themes.el
+++ standard-themes.el
@@ -2143,6 +2143,8 @@ color mappings instead of the complete palette."
     `(smerge-refined-removed ((,c :inherit diff-refine-removed)))
     `(smerge-upper ((,c :inherit diff-removed)))
 ;;;; spacious-padding
+    `(spacious-padding-line-active ((,c :foreground ,accent-0)))
+    `(spacious-padding-line-inactive ((,c :foreground ,border)))
     `(spacious-padding-subtle-mode-line-active ((,c :foreground ,accent-0)))
     `(spacious-padding-subtle-mode-line-inactive ((,c :foreground ,border)))
 ;;;; tab-bar-mode

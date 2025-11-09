77533335a07302c2c56e963e62d055364be8da48
Protesilaos Stavrou <info@protesilaos.com>
2025-11-09 07:46:00 +0200

    Be consistent with the file notice about where the themes are
    They are not in emacs.git or the tarball.

--- standard-dark-theme.el
+++ standard-dark-theme.el
@@ -7,7 +7,7 @@
 ;; URL: https://github.com/protesilaos/standard-themes
 ;; Keywords: faces, theme, accessibility
 
-;; This file is part of GNU Emacs.
+;; This file is NOT part of GNU Emacs.
 
 ;; GNU Emacs is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by

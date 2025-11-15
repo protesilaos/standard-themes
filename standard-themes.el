84b84893184c34a5fd912620c43e615a024c3249
Protesilaos Stavrou <info@protesilaos.com>
2025-11-15 13:56:38 +0200

    Use a more prominent background for the company and corfu popups
    Thanks to Vincenzo for making this suggestion in issue 8:
    <https://github.com/protesilaos/standard-themes/issues/8>.

--- standard-themes.el
+++ standard-themes.el
@@ -170,6 +170,8 @@ if you prefer to blend Standard and Modus into a single group, enable
 
 (defconst standard-themes-custom-faces
   '(
+    `(company-tooltip ((,c :background ,bg-alt)))
+    `(corfu-default ((,c :background ,bg-alt)))
     `(magit-section-highlight ((,c :background ,bg-alt)))
     `(magit-diff-file-heading-highlight ((,c :inherit magit-diff-file-heading :background ,bg-alt)))
     `(mode-line
